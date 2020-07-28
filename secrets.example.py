9c0b0a70c88d92ebc1b41b3d897b20d01de65890
Vijay Stroup <vijaystroup@gmail.com>
2020-07-28 13:30:22 -0400

    secrets established

---
+++ secrets.example.py
@@ -0,0 +1,9 @@
+secrets = {
+    'email': 'myemail@domain.com',
+    'password': 'mypassword',
+    'to': '0000000@domain.com'
+}
+# verizon: @vtext.com
+# at&t: @txt.att.net
+# sprint: @messaging.sprintpcs.com
+# tm: @tmomail.net
